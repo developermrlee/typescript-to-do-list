022a4ddd31e725d3f85c0343e92203567546d547
henry lee <djteski@gmail.com>
2024-06-24 22:21:46 -0500

    models folder for interfaces created. event handlers added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TodoList from './components/TodoList';
+import NewTodo from './components/NewTodo';
+import { Todo } from './models/todo.model';
 
 function App() {
-  const todos = [{ id: 't1', text: 'Finish the course' }]
+  const [todos, setTodos] = useState<Todo[]>([]);
+  
+  const todoAddHandler = (text: string) => {
+    setTodos(previousTodos => [
+      ...previousTodos, 
+      { id: Math.random().toString(), text: text }
+    ]);
+  };
+
+  const todoDeleteHandler = (todoId: string) => {
+    setTodos(previousTodos => {
+      return previousTodos.filter(todo => todo.id !== todoId);
+    });
+  }
+
   return (
     <div className="App">
-      <TodoList items={todos} />
+      <NewTodo onAddTodo={todoAddHandler} />
+      <TodoList items={todos} onDeleteTodo={todoDeleteHandler} />
     </div>
   );
 }
